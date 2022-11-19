1fad3a662635c003df468437dfe05fa295584331
mazajo90 <jorgemaza90@gmail.com>
2022-11-18 22:23:39 -0300

    wifipwn modification

--- wifipwn.sh
+++ wifipwn.sh
@@ -14,28 +14,36 @@ echo -e "${green}    \_/\_/   |_||_|  |_||_|      \_/\_/  |_| |_|${end}"
 echo -e "${green}                                                ${end}"
 echo -e "\n"
 
+
+
 function ctrl_c(){
 	echo -e "\n${red}[-]Saliendo!!${end}"
 	tput cnorm && exit 1
-
+	
 }
+
 
 trap ctrl_c INT
 
+sleep 2
+
 function helpPanel(){
          echo -e "\n${green}[+] Uso de la herramienta: $0${end}"
-         echo -e "\t${green}w) Colocar el nombre del SSID${end}"
+         echo -e "\t\n${green}w) Colocar el nombre del SSID${end}${yellow} (Ejemplo: $0 -w my_wifi)${end}"
 }
 
 function wifiPwn(){
+	tput civis
 	name="$1"
 	pwd="$(sudo cat /etc/NetworkManager/system-connections/$name | grep -vE "connection|wifi|wifi-security|ipv4|ipv6|proxy|key" | grep -E "psk" | awk '{print $1}' | tr -d "psk|=")"
 
 	if [ "$pwd" ]; then
 		echo -e "${yellow}El password es:${end} ${green}$pwd${end}"
 	else
-		echo -e "${red}Ese SSID es incorrecto${end}" 2>/dev/null
+		echo -e "${red}SSID incorrecto, proporcione uno valido${end}"
 	fi
+
+	tput cnorm
 }
 
 declare -i parameter_counter=0
